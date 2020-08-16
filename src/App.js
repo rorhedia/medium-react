641a7526c63bf799c0aada86689c47ad193c19df
rodrok7 <rodrok7@gmail.com>
2020-08-16 18:26:45 -0500

    estructura-form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
+import { Form, Input, InputNumber, Button } from "antd";
+import { DatePicker } from "antd";
+
 import "./App.css";
+import CustomForm from "./screens/CustomForm/";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CustomForm></CustomForm>
     </div>
   );
 }
