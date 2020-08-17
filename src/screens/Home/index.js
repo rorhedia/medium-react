8672ef3ab712f406ce2518f99a0bebdd95eed7f9
Ruben Somuano <rubasomuano.g2@gmail.com>
2020-08-17 00:40:44 -0500

    props

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -14,7 +14,7 @@ function Home() {
   const [posts, setPost] = useState([]);
 
   useEffect(() => {
-    fetch("https://reactsessions-ac545.firebaseio.com/equipouno.json")
+    fetch("https://reactsessions-ac545.firebaseio.com/equipotres.json")
       .then((res) => res.json())
       .then((data) => {
         let usersArr = [];
@@ -22,44 +22,42 @@ function Home() {
           data[key]["key"] = key;
           usersArr.push(data[key]);
         }
-        console.log(usersArr);
         setPost(usersArr);
       });
   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <br></br>
-        <Row className="containerCardTT">
-          <Col className="containerCRT" xs={24} sm={24} md={12} lg={9}>
-            <CustomCardL></CustomCardL>
-          </Col>
-          <Col className="containerCMT" xs={24} sm={24} md={12} lg={7}>
-            <CustomCard></CustomCard>
-            <CustomCard></CustomCard>
-            <CustomCard></CustomCard>
-          </Col>
-          <Col className="containerLT" xs={0} sm={0} md={0} lg={8}>
-            <CustomCardR></CustomCardR>
-          </Col>
-        </Row>
-        <p className="TextSee">SEE EDITOR’S PICKS ›</p>
-        <br></br>
-        <hr></hr>
-        <br></br>
-        <br></br>
-        <Row className="containerCIF">
-          <Col xs={24} sm={24} md={24} lg={16}>
-            <CustomCardI></CustomCardI>
-          </Col>
-          <Col xs={24} sm={24} md={24} lg={6}>
-            <p className="FechaCardItt">Popular on Medium</p>
-            <hr></hr>
-            <CustomCardList></CustomCardList>
-          </Col>
-        </Row>
-      </header>
-    </div>
+    <header className="App-header">
+      {console.log(posts, posts[1])}
+      <br></br>
+      <Row className="containerCardTT">
+        <Col className="containerCRT" xs={24} sm={24} md={12} lg={9}>
+          {posts ? <CustomCardL card={posts[0]} /> : null}
+        </Col>
+        <Col className="containerCMT" xs={24} sm={24} md={12} lg={7}>
+          <CustomCard />
+          <CustomCard />
+          <CustomCard />
+        </Col>
+        <Col className="containerLT" xs={0} sm={0} md={0} lg={8}>
+          <CustomCardR />
+        </Col>
+      </Row>
+      <p className="TextSee">SEE EDITOR’S PICKS ›</p>
+      <br></br>
+      <hr></hr>
+      <br></br>
+      <br></br>
+      <Row className="containerCIF">
+        <Col xs={24} sm={24} md={24} lg={16}>
+          <CustomCardI />
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={6}>
+          <p className="FechaCardItt">Popular on Medium</p>
+          <hr></hr>
+          <CustomCardList />
+        </Col>
+      </Row>
+    </header>
   );
 }
 
