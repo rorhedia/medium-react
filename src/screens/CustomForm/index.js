5507482a11384a2b3ddf0ec8b6f9bf54258345a9
rodrok7 <rodrok7@gmail.com>
2020-08-16 22:53:17 -0500

    form-finalizado

--- src/screens/CustomForm/index.js
+++ src/screens/CustomForm/index.js
@@ -1,43 +1,57 @@
 import React from "react";
 import "./CustomForm.css";
 import { Form, Input, InputNumber, Button } from "antd";
+import { Row, Col } from "antd";
 
 function CustomForm() {
   return (
-    <Form name="nest-messages">
-      <Form.Item
-        name={["user", "name"]}
-        label="Name"
-        rules={[{ required: true }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name={["user", "email"]}
-        label="Email"
-        rules={[{ type: "email" }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name={["user", "age"]}
-        label="Age"
-        rules={[{ type: "number", min: 0, max: 99 }]}
-      >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item name={["user", "website"]} label="Website">
-        <Input />
-      </Form.Item>
-      <Form.Item name={["user", "introduction"]} label="Introduction">
-        <Input.TextArea />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+    <body>
+      <Row className="containerForm">
+        <Col className="formD" span={10} push={2}>
+          <Form name="POST">
+            <Form.Item className="itemGenerico" label="Tittle">
+              <Input />
+            </Form.Item>
+            <Form.Item className="itemGenerico" label="Text">
+              <Input.TextArea />
+            </Form.Item>
+            <Form.Item className="itemGenerico" label="Image">
+              <Input />
+            </Form.Item>
+            <Form.Item className="itemGenerico" label="Author">
+              <Input />
+            </Form.Item>
+            <Form.Item className="itemGenerico" label="Date">
+              <Input />
+            </Form.Item>
+
+            <Row>
+              <Col span={12} push={8}>
+                <Form.Item>
+                  <Button type="primary" htmlType="submit">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Col>
+
+              <Col span={12}>
+                <Form.Item className="buttonDelete">
+                  <Button type="secondary" htmlType="submit">
+                    Delete
+                  </Button>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+        <Col span={14} push={3}>
+          <img
+            className="imagenFrom"
+            src="http://127.0.0.1:5503/images/1_90cu6c0u4ZL8H8ic1woZDw@2x.png"
+          ></img>
+        </Col>
+      </Row>
+    </body>
   );
 }
 
