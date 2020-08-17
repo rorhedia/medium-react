4f431804249e7ae59a82143d9538a7f6ef17a696
rodrok7 <rodrok7@gmail.com>
2020-08-17 00:48:03 -0500

    Form-1

--- src/screens/CustomForm/index.js
+++ src/screens/CustomForm/index.js
@@ -4,54 +4,60 @@ import { Form, Input, InputNumber, Button } from "antd";
 import { Row, Col } from "antd";
 
 function CustomForm() {
+  const createPost = (data) => {
+    let created = Math.floor(Date.now() / 1000);
+
+    data["created"] = created;
+    fetch("https://reactsessions-ac545.firebaseio.com/equipotres.json", {
+      method: "POST",
+      body: JSON.stringify(data),
+    }).then((response) => console.log(response));
+  };
   return (
-    <body>
-      <Row className="containerForm">
-        <Col className="formD" span={10} push={2}>
-          <Form name="POST">
-            <Form.Item className="itemGenerico" label="Tittle">
-              <Input />
-            </Form.Item>
-            <Form.Item className="itemGenerico" label="Text">
-              <Input.TextArea />
-            </Form.Item>
-            <Form.Item className="itemGenerico" label="Image">
-              <Input />
-            </Form.Item>
-            <Form.Item className="itemGenerico" label="Author">
-              <Input />
-            </Form.Item>
-            <Form.Item className="itemGenerico" label="Date">
-              <Input />
-            </Form.Item>
+    <Row className="containerForm">
+      <Col className="formD" span={10} push={2} xs={24} sm={24} md={12} lg={12}>
+        <Form onFinish={createPost}>
+          <Form.Item name="title" className="itemGenerico" label="Tittle">
+            <Input />
+          </Form.Item>
+          <Form.Item name="text" className="itemGenerico" label="Text">
+            <Input.TextArea />
+          </Form.Item>
+          <Form.Item name="author" className="itemGenerico" label="Author">
+            <Input />
+          </Form.Item>
+          <Form.Item name="urlimage" className="itemGenerico" label="Image">
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="paragraph"
+            className="itemGenerico"
+            label="Paragraph"
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item name="company" className="itemGenerico" label="Company">
+            <Input />
+          </Form.Item>
 
-            <Row>
-              <Col span={12} push={8}>
-                <Form.Item>
-                  <Button type="primary" htmlType="submit">
-                    Submit
-                  </Button>
-                </Form.Item>
-              </Col>
-
-              <Col span={12}>
-                <Form.Item className="buttonDelete">
-                  <Button type="secondary" htmlType="submit">
-                    Delete
-                  </Button>
-                </Form.Item>
-              </Col>
-            </Row>
-          </Form>
-        </Col>
-        <Col span={14} push={3}>
-          <img
-            className="imagenFrom"
-            src="http://127.0.0.1:5503/images/1_90cu6c0u4ZL8H8ic1woZDw@2x.png"
-          ></img>
-        </Col>
-      </Row>
-    </body>
+          <Row xs={24} sm={24} md={24} lg={24}>
+            <Col className="bottonR" span={12} push={11}>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Col>
+      <Col span={14} push={3} xs={24} sm={24} md={12} lg={12}>
+        <img
+          className="imagenFrom"
+          src="http://127.0.0.1:5503/images/1_90cu6c0u4ZL8H8ic1woZDw@2x.png"
+        ></img>
+      </Col>
+    </Row>
   );
 }
 
